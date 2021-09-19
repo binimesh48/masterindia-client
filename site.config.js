6454cf61ae888db82139bbac701a6dc0c0fd2314
binimesh48 <49594431+binimesh48@users.noreply.github.com>
2021-09-19 23:40:23 +0530

    initial commit

---
+++ site.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'tabnine',
+  description: 'Project',
+  siteUrl: '',
+  googleAnalytic: 'UA-149699646-1',
+  author: {
+    email: 'binimesh12@gmail.com',
+    name: 'binimesh kumar'
+  },
+  lang: 'en'
+}
